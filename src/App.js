5a18f8d20b8f6af5591b54cab3b8a6128ea443d0
Pete Looney <petelooney81@gmail.com>
2020-10-29 16:35:19 -0500

    react bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Nav
+        activeKey="/home"
+        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+      >
+        <Nav.Item>
+          <Nav.Link href="/home">PL</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">About Me</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Projects</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-3">Skills</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Hello My Name is Pete</h1>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
